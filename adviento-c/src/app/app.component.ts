5e6c5fe32064915466f3a59943c5666753bd4583
Laura Sofia Vergara Marquez <laurasvergaram@gmail.com>
2024-02-12 23:03:53 -0500

    :sparkles: Dia 1: La carta de Hogwarts perdida

--- adviento-c/src/app/app.component.ts
+++ adviento-c/src/app/app.component.ts
@@ -21,4 +21,45 @@ export class AppComponent implements OnInit {
   public boobstrap(index: number) {
     console.log('Hello World!' + index);
   }
+
+  public revelio(){
+    console.log("funcion revelio");
+    
+    const textoDecifrado = document.getElementById("textoDecifrado")
+    let palabraMagica = (document.getElementById("palabraMagica") as any).value
+    const cifrado = 'SMZRGMBZHJ EW UIEHJ HP TO DEBML, LCEHZ PZA GLIÑJW DM QFRQMPZHVR ZR CMOCMYEO G ZR EQIYBIIE IYYKO KIMQTVO. VR ZWEM ZLKVV XQGKIMMZAC P PGIYW RV QVVLDWCPVW, PVQFROVLZÁG RQDKZA ZVEGID, LSJEAÍSD MAFGDSYIBKIN C AWRVVZW BCS EM NMBCWVVV MXIUZRVW. ¡AZSGÁVVXP XOIE ZQMIFTEMXP MB LR QMLRS ZRJPGQRRFGI SIQZE GS OMGTSISNQRF, HJROM QRHV HÍL BS VWKICI IEE IYPDO JSMTCMGR C XEOI VVGCMKW HV PGIGIFÁ DÁW XICKO UI YIDKISVDV EC JVVYEOMFF TJXPVQZEG!.';
+    console.log(palabraMagica);
+    if (palabraMagica == 'revelio'){
+      if (textoDecifrado){
+      textoDecifrado.innerHTML = "Has revelado la siguiente frase: "+this.descifrarVigenere(cifrado, palabraMagica);
+    }
+    
+  }
+  else{
+    console.log("asdkajd " + textoDecifrado);
+    if (textoDecifrado){
+    textoDecifrado.innerHTML = "intenta de nuevo";
+    }
+  }
+}
+
+  private descifrarVigenere(cifrado: string, clave: string) {
+    clave = clave.toUpperCase().replace(/[^A-Z]/g, '');
+    let claveIndex = 0;
+    return Array.from(cifrado).map(char => {
+        char = char.toUpperCase();
+        if (/[A-Z]/.test(char)) {
+            const shift = clave.charCodeAt(claveIndex) - 65;
+            const charCode = ((char.charCodeAt(0) - 65 - shift + 26) % 26) + 65;
+            claveIndex = (claveIndex + 1) % clave.length;
+            return String.fromCharCode(charCode);
+        } else {
+            return char;
+        }
+    }).join('');
+  
+  }
+
+
+
 }
